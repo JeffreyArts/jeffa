fa727a0d4332cb076d40f2f59761f68738129709
Jeffrey <sjeffff@gmail.com>
2023-04-09 23:50:49 +0200

    Add possibility to create marvin chatrooms

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  "io": {
+    "enabled": true,
+    "config": {
+      "IOServerOptions" :{
+        "cors": { "origin": env('APP_ORIGIN', 'http://localhost:3000'), "methods": ["GET"] },
+      },
+      "contentTypes": {
+        // "message": "*",
+        "marvin-chat":["update", "create"]
+      },
+      "events":[
+        {
+          "name": "connection",
+          "handler": ({ strapi }, socket) => {
+            strapi.log.info(`[io] new connection with id ${socket.id}`);
+          },
+        },
+      ]
+    },
+  },
+});
