ec753a941a57c0ace74325bd359ae1e19f8a9207
Jeffrey <sjeffff@gmail.com>
2023-04-14 23:51:20 +0200

    Add pm2 for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  apps: [{
+    name: 'jeffa-api',
+    script: 'nvm use 16 && yarn',
+    args: 'start',
+    cwd: process.env.DEPLOYMENT_CWD,
+    env: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: process.env.DEPLOYMENT_USER,
+      host: process.env.DEPLOYMENT_HOST,
+      ref: 'origin/main',
+      repo: 'git@github.com:JeffreyArts/jeffa.git',
+      path: process.env.DEPLOYMENT_PATH,
+      'post-deploy': 'nvm use 16 yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
