b407d7fc77446fcd5a09299395b4a1db55503d23
Jeffrey Arts <sjeffff@gmail.com>
2023-11-27 13:32:18 +0100

    Replace default url

--- src/api/ian-lisa-gasten/content-types/ian-lisa-gasten/lifecycles.js
+++ src/api/ian-lisa-gasten/content-types/ian-lisa-gasten/lifecycles.js
@@ -3,14 +3,14 @@ module.exports = {
         const { result, params  } = event;
 
         // If no custom url is set, generate one
-        if (event.params.data.url == "https://www.05-07-2024.nl/uitnodiging/") {
+        if (event.params.data.url == "") {
             const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
             const length = 8;
             let randomString = '';
             for (let i = 0; i < length; i++) {
                 randomString += chars.charAt(Math.floor(Math.random() * chars.length));
             }
-            event.params.data.url = `https://www.05-07-2024.nl/uitnodiging/${randomString}` 
+            event.params.data.url = `https://www.ianlisa.nl/uitnodiging/${randomString}` 
         }
     },
 };
